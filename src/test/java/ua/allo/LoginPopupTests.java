fa511167a47650d4e9e949e81ec0f3ef7810d69f
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2022-10-23 21:35:17 +0300

    Extend Success Login Test. Add Login/LogoutTest

--- src/test/java/ua/allo/LoginPopupTests.java
+++ src/test/java/ua/allo/LoginPopupTests.java
@@ -1,22 +1,46 @@
 package ua.allo;
 
+import com.codeborne.selenide.Condition;
 import core.ChromeDriverWeb;
 import core.LoginPopup;
 import org.junit.Test;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
 import static core.MainPage.*;
 
 
 public class LoginPopupTests extends ChromeDriverWeb {
 
+
     @Test
     public void successLoginTest() {
+        var welcomeText = "Вітаємо, Test";
+
         new userProfile()
                 .openLoginPopup();
         new LoginPopup()
                 .fillLogin("tmp.test.user.aqa@gmail.com")
                 .fillPassword("IvCUZ4qv5")
                 .clickOnLogin();
+        new userProfile()
+                .openUserProfileMenu()
+                .openInfoMenu();
+        $(".customer-account .customer-account__title").shouldHave(Condition.text(welcomeText));
+    }
+
+    @Test
+    public void LoginLogoutTest() {
+        var logoutText = "Ви вийшли зі свого аккаунта";
+        new userProfile()
+                .openLoginPopup();
+        new LoginPopup()
+                .fillLogin("tmp.test.user.aqa@gmail.com")
+                .fillPassword("IvCUZ4qv5")
+                .clickOnLogin();
+        new userProfile()
+                .openUserProfileMenu()
+                .logout();
+        $(".customer-logout .customer-logout__title").shouldHave(Condition.text(logoutText));
     }
 }
