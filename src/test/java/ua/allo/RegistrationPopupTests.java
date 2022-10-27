31a20d6c7b786c1b6ecd895c5bcdc1d20f5b1a3c
vladimir.tiuliukov <tiuliukov.v@gmail.com>
2022-10-27 22:01:36 +0300

    Extend Registration tests

--- src/test/java/ua/allo/RegistrationPopupTests.java
+++ src/test/java/ua/allo/RegistrationPopupTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class RegistrationPopupTests extends BaseConfig{
     @Test
-    public void RegistrationTest() {
+    public void RequiredFieldsTest() {
         var EmptyFieldError = "Це поле є обов'язковим для заповнення.";
 
         new MainPage.userProfile()
@@ -19,9 +19,44 @@ public class RegistrationPopupTests extends BaseConfig{
                 .openRegistrationPopup();
         new RegistrationPopup()
                 .clickOnRegistration();
-        $(".validation-advice.v-validation-error div").shouldHave(Condition.text(EmptyFieldError));
-        $(".validation-advice.v-validation-error div").shouldHave(Condition.text(EmptyFieldError));
+        $(".validation-list .validation-text").shouldHave(Condition.text(EmptyFieldError));
+        $(".modal-input:nth-child(2) .validation-advice").shouldHave(Condition.text(EmptyFieldError));
+        $(".modal-input:nth-child(4) .validation-advice").shouldHave(Condition.text(EmptyFieldError));
+    }
 
+    @Test
+    public void SendRegistrationRequestTest() {
+        var description = "Введіть код підтвердження з sms / viber, відправлений на номер";
+        var testPhone = "0511112233";
+        var testName = "Test";
+        var testPass = "Qwerty";
+        new MainPage.userProfile()
+                .openLoginPopup();
+        new LoginPopup()
+                .openRegistrationPopup();
+        new RegistrationPopup()
+                .fillName(testName)
+                .fillPhone(testPhone)
+                .fillPassword(testPass)
+                .clickOnRegistration();
+        $("#login-form .code-verification-text.verification-text-last").shouldHave(Condition.text(description));
+    }
 
+    @Test
+    public void AlreadyRegisterTest() {
+        var description = "Користувач з номером +380985019780 вже зареєстрований. Будь ласка авторизуйтесь.";
+        var testPhone = "0985019780";
+        var testName = "Test";
+        var testPass = "Qwerty";
+        new MainPage.userProfile()
+                .openLoginPopup();
+        new LoginPopup()
+                .openRegistrationPopup();
+        new RegistrationPopup()
+                .fillName(testName)
+                .fillPhone(testPhone)
+                .fillPassword(testPass)
+                .clickOnRegistration();
+        $(".modal-input.validation-error .validation-advice.v-validation-error").shouldHave(Condition.text(description));
     }
 }
